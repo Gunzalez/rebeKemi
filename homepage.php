e98e8d45374043f866df88b7b4914db340bd0bad
Segun Konibire <gunzalez@hotmail.com>
2019-10-02 00:22:19 +0100

    style carousel on homepage template

--- homepage.php
+++ homepage.php
@@ -9,27 +9,27 @@ get_header(); ?>
         <div class="carousel-cell" style="background-image: url('<?php the_field('slide_1_background'); ?>')">
             
             <div class="wrectangle">
-                <h3 class="title"><?php the_field('slide_1_title'); ?></h3>
-                <p class="call-out"><?php the_field('slide_1_body'); ?></p>
+                <h3 class="title"><?php the_field('slide-1-title'); ?></h3>
+                <p class="call-out"><?php the_field('slide_1_paragraph'); ?></p>
                 <a class="link" href="<?php the_field('slide_1_destination'); ?>"><?php the_field('slide_1_link_text'); ?></a>
             </div>
 
         </div>
-        <div class="carousel-cell">
+        <div class="carousel-cell" style="background-image: url('<?php the_field('slide_2_background'); ?>')">
             
             <div class="wrectangle">
-                <h3 class="title">Donations</h3>
-                <p class="call-out">Suspendisse sit amet nisi est. Sed consequat, velit grolla.</p>
-                <a class="link" href="#">How you can help</a>
+                <h3 class="title"><?php the_field('slide-2-title'); ?></h3>
+                <p class="call-out"><?php the_field('slide_2_paragraph'); ?></p>
+                <a class="link" href="<?php the_field('slide_2_destination'); ?>"><?php the_field('slide_2_link_text'); ?></a>
             </div>
 
         </div>
-        <div class="carousel-cell">            
+        <div class="carousel-cell" style="background-image: url('<?php the_field('slide_3_background'); ?>')">            
             
             <div class="wrectangle">
-                <h3 class="title">The Blog</h3>
-                <p class="call-out">Join in discussions, vel gravida urna placerat ac.</p>
-                <a class="link" href="#">Join the discussions</a>
+                <h3 class="title"><?php the_field('slide-3-title'); ?></h3>
+                <p class="call-out"><?php the_field('slide_3_paragraph'); ?></p>
+                <a class="link" href="<?php the_field('slide_3_destination'); ?>"><?php the_field('slide_3_link_text'); ?></a>
             </div>
 
         </div>
