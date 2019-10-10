b756c7516c518e78363e94627a858c7b57e98f75
Segun Konibire <gunzalez@hotmail.com>
2019-10-11 00:51:06 +0100

    tweaks single page and post templates

--- page.php
+++ page.php
@@ -1,20 +1,40 @@
 <?php
 /**
- * The template for displaying all single posts.
+ * The template for displaying all single pages.
  *
  * @package Stacker
  */
 
 get_header(); ?>
+					custom single page include
 
 	<div class="masonryinside">
 		<div class="wrapper">
 			<?php while ( have_posts() ) : the_post(); ?>
 
+				<!-- includes file directly -->
+				<div <?php post_class( 'inside item' ); ?>>
+					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'post-page', array( 'class' => 'featured-image' ) ); ?></a>
 
-			PAGE
+					<div class="commentcount">
+						<?php if ( !post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+							<span><?php comments_popup_link( '0', '1', '%' ); ?></span>
+						<?php endif; ?>
+					</div>
+					<h2 class="itemtitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+					<div class="itemdate"><a href="<?php the_permalink(); ?>"><?php stacker_posted_on(); ?></a></div>
+					<div class="itemcat">
+				<?php the_category( ' ' ); ?>
+					</div>
+					<div id="content">
+						<?php
+							wp_link_pages();
+							the_content();
+							edit_post_link( __( 'Edit', 'stacker-lite' ), '<span class="edit-link">', '</span>' );
+						?>
+					</div>
+				<!-- includes file directly -->
 
 
 				<?php
--- single.php
+++ single.php
@@ -10,11 +10,33 @@ get_header(); ?>
 	<div class="masonryinside">
 		<div class="wrapper">
 			<?php while ( have_posts() ) : the_post(); ?>
+			
 
-			SINGLE
+				<!-- includes file directly -->				
+				<div <?php post_class( 'inside item' ); ?>>
+					
+					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'post-page', array( 'class' => 'featured-image' ) ); ?></a>
+					<div class="commentcount">
+							<span><?php comments_number( '0', '1', '%' ); ?></span>
+					</div>
+					<h2 class="itemtitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-				<?php get_template_part( 'content', 'single' ); ?>
-
+					<div class="itemdate"><a href="<?php the_permalink(); ?>"><?php stacker_posted_on(); ?></a></div>
+					<div class="itemcat">
+				<?php the_category( ' ' ); ?>
+					</div>
+					<div id="content">
+						<?php
+							wp_link_pages();
+							the_content();
+							posts_nav_link( ' &#183; ', 'previous page', 'next page' );
+							edit_post_link( __( 'Edit', 'stacker-lite' ), '<span class="edit-link">', '</span>' );
+						?>
+						<div id="bottommeta">
+							<?php the_tags( __( 'Tags: ', 'stacker-lite' ), '  ', '' ); ?>
+						</div>
+					</div>
+				<!-- includes file directly -->
 
 
 				<?php
