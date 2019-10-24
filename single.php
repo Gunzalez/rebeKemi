be6056df7e177395ea4b037eb0dbac073fcf43cc
Segun Konibire <gunzalez@hotmail.com>
2019-10-24 01:50:05 +0100

    building and styling the single page

--- single.php
+++ single.php
@@ -6,48 +6,65 @@
  */
 
 get_header(); ?>
-single page include
 
-	<div class="masonryinside">
-		<div class="wrapper">
-			<?php while ( have_posts() ) : the_post(); ?>
-			
+	
+	<?php while ( have_posts() ) : the_post(); ?>
 
-				<!-- includes file directly -->				
-				<div <?php post_class( 'inside item' ); ?>>
-					
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'post-page', array( 'class' => 'featured-image' ) ); ?></a>
-					<div class="commentcount">
-							<span><?php comments_number( '0', '1', '%' ); ?></span>
-					</div>
-					<h2 class="itemtitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-					<div class="itemdate"><a href="<?php the_permalink(); ?>"><?php stacker_posted_on(); ?></a></div>
-					<div class="itemcat">
-				<?php the_category( ' ' ); ?>
-					</div>
-					<div id="content">
-						<?php
-							wp_link_pages();
-							the_content();
-							posts_nav_link( ' &#183; ', 'previous page', 'next page' );
-							edit_post_link( __( 'Edit', 'stacker-lite' ), '<span class="edit-link">', '</span>' );
-						?>
-						<div id="bottommeta">
-							<?php the_tags( __( 'Tags: ', 'stacker-lite' ), '  ', '' ); ?>
+		<?php if (has_post_thumbnail() ) { ?>				
+				<div class="page-header hero" 
+				style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>')">&nbsp;</div>
+		<?php } else { ?>
+				<div class="page-header naked">              
+					<div class="wrectangle">
+						<h2 class="title"><?php the_title(); ?></h2>
+						<h3 class="date-posted"><?php stacker_posted_on(); ?></h3>
+						<div class="itemcat">
+							<?php the_category( ' ' ); ?>
 						</div>
 					</div>
-				<!-- includes file directly -->
+				</div>
+		<?php } ?>
 
+		<div class="masonryinside no-margin">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9 col-md-6 offset-md-2">
+						<div class="the_content">						
+							<?php if (has_post_thumbnail() ) { ?>
+								<h3 class="title"><?php the_title(); ?></h3>
+								<h3 class="date-posted"><?php stacker_posted_on(); ?></h3>
+								<div class="itemcat">
+									<?php the_category( ' ' ); ?>
+								</div>
+							<?php } ?>					
+							<?php the_content(); ?>
 
-				<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() ) :
-					comments_template();
-				endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
+						</div>
+					</div>
+					<div class="col-sm-3 col-md-2">
+						<div class="my-stories">						
+							<?php echo wpb_list_child_pages(); ?>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
+	
+			
+			
+
+
+	<?php endwhile; // end of the loop. ?>
+
+	
 <?php get_footer(); ?>
