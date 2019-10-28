94d121ff939e96c5447c7513b2ca041c14652bbb
Segun Konibire <gunzalez@hotmail.com>
2019-10-28 00:20:29 +0000

    building out custom post pods

---
+++ custom-content.php
@@ -0,0 +1,15 @@
+<div <?php post_class( 'item custom-item' ); ?>>
+	<a href="<?php the_permalink(); ?>">
+		<?php if ( has_post_thumbnail() ) {?>
+			<?php the_post_thumbnail( 'post-thumb', array( 'class' => '' ) ); ?>
+		<?php } else { ?>
+			<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) ?>/img/ribbon.jpg" title="Article"/>
+		<?php } ?>
+	</a>
+	<div class="copy">
+		<h2 class="itemtitle"><?php the_title(); ?></h2>
+		<div class="itemdate"><?php stacker_posted_on() ?></div>
+		<div class="itemcat"><?php the_category( ' ' ); ?></div>
+		<div class="comments"><span><?php comments_number( '0', '1', '%' ); ?></span></div>
+	</div>
+</div><!--End Post -->
