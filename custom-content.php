40e417fd85a0103c068ed8cca8ab16850200aa97
Segun Konibire <gunzalez@hotmail.com>
2020-05-29 16:53:38 +0100

    Styles blog boxes

--- custom-content.php
+++ custom-content.php
@@ -1,18 +1,26 @@
+<?php
+$islarger = ''
+?>
 <div <?php post_class( 'item custom-item' ); ?>>
 	<a href="<?php the_permalink(); ?>">
+
 		<?php if ( has_post_thumbnail() ) {?>
+		
 			<?php the_post_thumbnail( 'post-thumb', array( 'class' => '' ) ); ?>
+
 		<?php } else { ?>
 			
+			<?php $islarger = 'larger-title' ?>
 			<div class='no-image'>
-				<span class='logo-link'><span class='rebe'>rebe</span><span class='kemi'>Kemi</span>
+				<span class='logo-link'>
+					<span class='rebe'>rebe</span><span class='kemi'>Kemi</span>
 				</span>
 			</div>
 			
 		<?php } ?>
 	</a>
 	<div class="copy">
-		<h2 class="itemtitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="itemtitle <?php echo $islarger ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<div class="itemdate"><?php stacker_posted_on() ?></div>
 		<div class="itemcat"><?php the_category( ' ' ); ?></div>
 		<div class="comments"><span><?php comments_number( '0', '1', '%' ); ?></span></div>
