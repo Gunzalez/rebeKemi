665ffbdd4db40e6ba4fae3c4ecb369a315d59b63
Segun Konibire <gunzalez@hotmail.com>
2021-01-23 18:34:37 +0000

    Refactor

--- custom-content-intro.php
+++ custom-content-intro.php
@@ -1,9 +1,7 @@
 <div <?php post_class( 'item custom-item' ); ?>>
-
 	<div class="copy">
 		<h2 class="itemtitle larger-title item-link"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <p class="intro-link"><?php the_field('introduction'); ?></p>
+        <a href="<?php the_permalink(); ?>" class="intro-link"><?php the_field('introduction'); ?></a>
 		<div class="comments"><span><?php comments_number( '0', '1', '%' ); ?></span></div>
 	</div>
-
 </div><!--End Post -->
